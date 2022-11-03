9a2e2164342c160266e5cc6e80efc254706b5c89
hellolutar <hellolutar@163.com>
2022-11-03 10:32:32 +0800

    add some books

---
+++ pull_ebooks_from_server.sh
@@ -0,0 +1,2 @@
+#/usr/bin/bash
+rsync -arv username@host:/data/docker_volumes/docker-nginx/files/ebooks/ ebooks
---
+++ push_ebooks_to_server.sh
@@ -0,0 +1,2 @@
+#/usr/bin/bash
+rsync -arv ebooks/ username@host:/data/docker_volumes/docker-nginx/files/ebooks
